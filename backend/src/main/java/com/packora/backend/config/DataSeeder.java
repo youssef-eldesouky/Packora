8071adb9266a1ec97f26b17744c0160b51ca9082
youssef-eldesouky <youssefeldesouky14@gmail.com>
2026-05-20 02:36:16 +0300

    add data seeder for default support users

--- backend/src/main/java/com/packora/backend/config/DataSeeder.java
+++ backend/src/main/java/com/packora/backend/config/DataSeeder.java
@@ -25,7 +25,7 @@ public class DataSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         seedAdminUser();
-        // seedSupportUsers();
+        seedSupportUsers();
     }
 
     private void seedAdminUser() {
@@ -41,6 +41,36 @@ public class DataSeeder implements CommandLineRunner {
             log.info("Default Admin user seeded successfully.");
         } else {
             log.debug("Admin user {} already exists. Skipping seeding.", adminEmail);
+        }
+    }
+
+    private void seedSupportUsers() {
+        String support1Email = "support.packora1@gmail.com";
+        if (!userRepository.existsByEmail(support1Email)) {
+            log.info("Seeding default Support user 1: {}", support1Email);
+            SupportStaff support1 = new SupportStaff();
+            support1.setUsername("support1");
+            support1.setEmail(support1Email);
+            support1.setPassword(passwordEncoder.encode("Packora@Support2026"));
+            support1.setShiftTime("DAY");
+            userRepository.save(support1);
+            log.info("Default Support user 1 seeded successfully.");
+        } else {
+            log.debug("Support user {} already exists. Skipping seeding.", support1Email);
+        }
+
+        String support2Email = "support.packora2@gmail.com";
+        if (!userRepository.existsByEmail(support2Email)) {
+            log.info("Seeding default Support user 2: {}", support2Email);
+            SupportStaff support2 = new SupportStaff();
+            support2.setUsername("support2");
+            support2.setEmail(support2Email);
+            support2.setPassword(passwordEncoder.encode("Packora@Support2026"));
+            support2.setShiftTime("NIGHT");
+            userRepository.save(support2);
+            log.info("Default Support user 2 seeded successfully.");
+        } else {
+            log.debug("Support user {} already exists. Skipping seeding.", support2Email);
         }
     }
 }
